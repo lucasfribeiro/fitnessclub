69dc0910956778c19ad52267f6c7734fe06bd70e
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2025-02-25 15:23:18 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-3xl">
+      <h1 className="font-roboto">texto com roboto</h1>
+      <h1 className="font-anton"> texto com anton</h1>
+    </div>
+  );
+}
